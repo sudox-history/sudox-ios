22471289259376997d3c49135a737c3ccdc05b0f
Ivan Lobanov <ia-lobanov@ya.ru>
2020-04-22 20:29:32 +0300

    Initial Commit

---
+++ sudox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sudox
+//
+//  Created by Иван Лобанов on 22/04/2020.
+//  Copyright © 2020 Sudox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
